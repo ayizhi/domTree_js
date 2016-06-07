0b6023431a344e1e0d5da815484cb477f90a8110
yizhi <zhangyiizhi@126.com>
2016-06-07 23:23:52 +0800

    going on

--- domTree.js
+++ domTree.js
@@ -77,12 +77,12 @@ var TreeData = {
                 },
                 {
                     "id": "7b84d91891de464e807d50bb0ad7619c",
-                    "admin_id": "0",
-                    "admin_name": "",
+                    "admin_id": "d461b77bc762411e8ca73545",
+                    "admin_name": "大马猴",
                     "name": "测试一部2",
                     "parent_id": "d461b77bc762411e8ca7d886e796ea5c",
                     "employee_count": 1,
-                    "children_ids": ['ae627ddfa006473a93574bcc322e4daas'],
+                    "children_ids": ['ae627ddfa00647sdfa93574bcc322efaf'],
                     "children": [
                         {
                             "children": [
@@ -117,7 +117,102 @@ var TreeData = {
                                             "children_ids": []
                                         }
                                     ]
-                                }
+                                },
+                                {
+                                    "id": "ae627ddfa00647sdfa93574bcc322efaf",
+                                    "admin_id": "0",
+                                    "admin_name": "",
+                                    "name": "那几句话关键是3",
+                                    "parent_id": "7b84d91891de464e807d50bb0ad7619c",
+                                    "employee_count": 2,
+                                    "children_ids": [
+                                        "0db513c2c18d4basde6a4e97cbb3dc5c5342",
+                                        "0db513c2c18d4asdbe6a4e97cbb3dc5c4521"
+                                    ],
+                                    "children": [
+                                        {
+                                            "id": "0db513c2c18d4basde6a4e97cbb3dc5c5342",
+                                            "admin_id": "0",
+                                            "admin_name": "",
+                                            "name": "三3",
+                                            "parent_id": "ae627ddfa00647sdfa93574bcc322efaf",
+                                            "employee_count": 0,
+                                            "children_ids": []
+                                        },
+                                        {
+                                            "id": "0db513c2c18d4asdbe6a4e97cbb3dc5c4521",
+                                            "admin_id": "0",
+                                            "admin_name": "",
+                                            "name": "四3",
+                                            "parent_id": "ae627ddfa00647sdfa93574bcc322efaf",
+                                            "employee_count": 0,
+                                            "children_ids": []
+                                        }
+                                    ]
+                                },
+                                {
+                                    "id": "ae627ddfa006dfhghda93574bcc322efaf",
+                                    "admin_id": "0",
+                                    "admin_name": "",
+                                    "name": "那几句话关键是4",
+                                    "parent_id": "7b84d91891de464e807d50bb0ad7619c",
+                                    "employee_count": 2,
+                                    "children_ids": [
+                                        "0db513c2c18d4basdsdae6a4e97cbb3dc5c5342",
+                                        "0db513c2csds18d4asdbe6a4e97cbb3dc5c4521"
+                                    ],
+                                    "children": [
+                                        {
+                                            "id": "0db513c2c18d4basdsdae6a4e97cbb3dc5c5342",
+                                            "admin_id": "0",
+                                            "admin_name": "",
+                                            "name": "三3",
+                                            "parent_id": "ae627ddfa006dfhghda93574bcc322efaf",
+                                            "employee_count": 0,
+                                            "children_ids": []
+                                        },
+                                        {
+                                            "id": "0db513c2csds18d4asdbe6a4e97cbb3dc5c4521",
+                                            "admin_id": "0",
+                                            "admin_name": "",
+                                            "name": "四3",
+                                            "parent_id": "ae627ddfa006dfhghda93574bcc322efaf",
+                                            "employee_count": 0,
+                                            "children_ids":[],
+                                            //"children_ids": ['0db513c2c18d4basdsdae6a4e97cbb3dc5c5342','0db513c2c18d4basdsdae6a4e97cbb3dc5c5342'],
+                                            //"children":[
+                                            //    {
+                                            //        "id": "0db513c2c18d4basdsdae6a4e97cbb3dc5c5342",
+                                            //        "admin_id": "0",
+                                            //        "admin_name": "",
+                                            //        "name": "三3",
+                                            //        "parent_id": "ae627ddfa006dfhghda93574bcc322efaf",
+                                            //        "employee_count": 0,
+                                            //        "children_ids": []
+                                            //    },
+                                            //    {
+                                            //        "id": "0db513c2c18d4basdsdae6a4e97cbb3dc5c5342",
+                                            //        "admin_id": "0",
+                                            //        "admin_name": "",
+                                            //        "name": "三3",
+                                            //        "parent_id": "ae627ddfa006dfhghda93574bcc322efaf",
+                                            //        "employee_count": 0,
+                                            //        "children_ids": []
+                                            //    },
+                                            //]
+                                        }
+                                    ]
+                                },
+                                //{
+                                //    "id": "0db513c2c18d4basdsdae6a4e97cbb3dc5c5342",
+                                //    "admin_id": "0",
+                                //    "admin_name": "",
+                                //    "name": "三3",
+                                //    "parent_id": "ae627ddfa006dfhghda93574bcc322efaf",
+                                //    "employee_count": 0,
+                                //    "children_ids": []
+                                //},
+
                             ],
                             "id": "7ffceb278dae4ab69d28b208f663a8ffa",
                             "admin_id": "0",
@@ -138,10 +233,18 @@ var TreeData = {
     }
 }
 
-var treeHtml = makeTreeHtml(TreeData.data.departmentStructure)
+var treeHtml = initMakeTreeHtml(TreeData.data.departmentStructure,'organization-tree')
 
-function makeTreeHtml(data){
-    var tmpHtml = '<ul>';
+$('#dom-tree').html(treeHtml);
+
+dealWithTreeStyle('#organization-tree')
+
+
+
+
+//domTree初始化,需要结束后塞入html骨架
+function initMakeTreeHtml(data,select){
+    var tmpHtml = '<ul  id="' + select + '" >';
 
     iterator(data)
 
@@ -149,19 +252,37 @@ function makeTreeHtml(data){
         var id = data.id;
         var name = data.name;
         var children = data.children;
+        var children_ids = data.children_ids;
+        var parent_id = data.parent_id;
+        var admin_id = data.admin_id;
+        var admin_name = data.admin_name ? data.admin_name: '负责人空缺';
+        var nodeContent = parent_id != '0' ? '<div class="department-name node-content">' + name + '</div><div class="people-num node-content">(' + children_ids.length + '人)</div><div class="admin-info node-content" data-admin_id="' + admin_id + '">(' + admin_name + ')</div>' + '<div class="closeNodeBtn"></div>' : '<div class="department-name node-content">' + name + '</div>';
+        var addAdminBtn = ((admin_id == '0') && (parent_id != '0')) ? '<div class="add-admin-btn"></div>' : '';
+        var liClassName = ''
+        var tmpThis = '';//每一次的临时字段
 
-        tmpHtml += '<ul><li>' + name + '</li>'
+        //处理li的className
+        liClassName += children_ids.length == 0 ? 'last-children' : '';
+        liClassName += parent_id == '0' ? 'original-node' : '';
 
+        //由parent与children决定头顶竖线
+        if(children_ids.length == 0){
+            tmpThis = '<ul data-id="'+ id +'"><div class="node-line top-line"><p class="left-line"></p><p class="right-line"></p></div><li class="'+ liClassName +'">' + nodeContent + addAdminBtn + '</li>'
+        }else if(parent_id == '0'){
+            tmpThis = '<ul data-id="'+ id +'"><li class="' + liClassName + '">' + nodeContent + addAdminBtn + '</li><div class="node-line bottom-line"><p class="left-line"></p><p class="right-line"></p></div>'
+        }else{
+            tmpThis = '<ul data-id="'+ id +'"><div class="node-line top-line"><p class="left-line"></p><p class="right-line"></p></div><li class="'+ liClassName +'">' + nodeContent + addAdminBtn + '</li><div class="node-line bottom-line"><p class="left-line"></p><p class="right-line"></p></div>'
+        }
+
+
+        tmpHtml += tmpThis
 
         if(children){
-
             for(var i= 0,len=children.length;i<len;i++){
                 var thisChildren = children[i];
                 iterator(thisChildren)
             }
-
             tmpHtml += '</ul>'
-
         }else{
             tmpHtml += '</ul>'
             return
@@ -172,6 +293,119 @@ function makeTreeHtml(data){
     return tmpHtml
 }
 
+//调整树状图样式(目前主要是横向)(插入节点,删除节点每次都需要)
+function dealWithTreeStyle(select){
+    var treeList = $(select);
+    treeIterator(treeList);
+    function treeIterator(dom){
+        var $childUl = $(dom).children('ul');
+        if($childUl.length > 1){
+            //处理横向连线样式
+            $childUl.each(function (index) {
+                if(index == 0){
+                    $(this).children('.top-line').children('.right-line').css('border-top','2px dashed #ccc')
+                }else if(index == $childUl.length - 1){
+                    $(this).children('.top-line').children('.left-line').css('border-top','2px dashed #ccc')
+                }else{
+                    $(this).children('.top-line').children('.right-line').css('border-top','2px dashed #ccc')
+                    $(this).children('.top-line').children('.left-line').css('border-top','2px dashed #ccc')
+                }
+            })
+        }else{
+            $childUl.each(function () {
+                $(this).children('.top-line').children('.right-line').css('border-top','none')
+                $(this).children('.top-line').children('.left-line').css('border-top','none')
+            })
+        }
+        if($childUl.length == 0){
+            return
+        }else{
+            $childUl.each(function () {
+                treeIterator($(this))
+            })
+        }
+
+    }
+}
+
+//关闭分支
+function closeNode(id){
+    var $parentNode = $('ul[data-id="'+ id +'"]');
+    var $tBottomLine = $parentNode.find('.bottom-line');
+    var $ul = $parentNode.find('ul');
+    $tBottomLine.css("visibility","hidden");
+    $ul.css("visibility","hidden");
+}
+//打开分支
+function openNode(id){
+    var $parentNode = $('ul[data-id="'+ id +'"]');
+    var $tBottomLine = $parentNode.find('.bottom-line');
+    var $ul = $parentNode.find('ul');
+    $tBottomLine.css("visibility","visible");
+    $ul.css("visibility","visible");
+}
+
+//插入分支
+function insertNode(id,obj){
+    var $targetNode = $('ul[data-id="'+ id +'"]');
+    var $siblings = $targetNode.children('ul');
+    var id = obj.id;
+    var name = obj.name;
+    var children = obj.children;
+    var children_ids = obj.children_ids;
+    var parent_id = obj.parent_id;
+    var admin_id = obj.admin_id ? obj.admin_id: '';
+    var admin_name = obj.admin_name ? obj.admin_name: '负责人空缺';
+    var addAdminBtn = ((admin_id == '0') && (parent_id != '0')) ? '<div class="add-admin-btn"></div>' : '';
+    var $newNode = $('<ul data-id="'+ id +'"><div class="node-line top-line"><p class="left-line"></p><p class="right-line"></p></div><li class="last-children">' + '<div class="department-name node-content">' + name + '</div><div class="people-num node-content">(' + children_ids.length + '人)</div><div class="admin-info node-content" data-admin_id="' + admin_id + '">(' + admin_name + ')</div>' + addAdminBtn + '</li></ul>');
+
+    //插入没有儿子的节点,需要给当前插入一个竖着的线
+    if($siblings.length == 0){
+        //去除类名
+        $targetNode.children('li').removeClass('last-children');
+        //插入竖线
+        $targetNode.append($('<div class="node-line bottom-line"><p class="left-line"></p><p class="right-line"></p></div>'));
+    }
+
+    $targetNode.append($newNode);
+    dealWithTreeStyle('#organization-tree');
 
 
-console.log(treeHtml)
+}
+
+//删除分支
+function deleteNode(id){
+    var $targetNode = $('ul[data-id="' + id + '"]');
+    var $paNode = $targetNode.parent('ul');
+    var $paNodeLi = $paNode.children('li');
+    var paId = $paNode.attr('data-id');
+    var $siblings = $targetNode.siblings('ul');
+
+    //没有兄弟节点,需要给paNode加上'last-children'的类名,并且去掉底部竖线
+    if($siblings.length == 0){
+        $paNodeLi.addClass('last-children');
+        $paNode.children('.bottom-line').remove();
+    }
+    $targetNode.remove();
+    dealWithTreeStyle('#organization-tree');
+}
+
+
+$(document).on('click','li .closeNodeBtn', function () {
+    var $thisBtn = $(this);
+    var $pa = $(this).parent('li');
+    var $grand = $pa.parent('ul');
+    var id = $grand.attr('data-id');
+
+    if(!$thisBtn.hasClass('close')){
+        closeNode(id);
+        $thisBtn.addClass('close');
+    }else{
+        openNode(id);
+        $thisBtn.removeClass('close');
+    }
+
+
+})
+
+
